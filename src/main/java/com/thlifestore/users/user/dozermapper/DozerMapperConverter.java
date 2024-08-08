91b0915523d729109c240feaf044598a4383b455
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2024-08-08 11:49:02 -0300

    create dozermapper

---
+++ src/main/java/com/thlifestore/users/user/dozermapper/DozerMapperConverter.java
@@ -0,0 +1,25 @@
+package com.thlifestore.users.user.dozermapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
+
+public class DozerMapperConverter {
+
+	public static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+	
+	
+	public static <O,D>D parseObjectForEntity(O origin,Class<D> destination){
+		return mapper.map(origin, destination);
+	}
+	
+	public static <O,D> List<D>parseListObjectForEntity(List<O>origin, Class<D>destination){
+		List<D> list  = new ArrayList<>();
+		for(O o : origin){
+			list.add(mapper.map(o,destination));
+		}
+		return list;
+	}
+}
