060d8f300172c352f44dd88db714ce4cd708dfef
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2024-08-09 15:06:45 -0300

    creating swagger customized

--- src/main/java/com/thlifestore/users/user/controller/UserController.java
+++ src/main/java/com/thlifestore/users/user/controller/UserController.java
@@ -17,8 +17,16 @@ import com.thlifestore.users.user.service.UserService;
 import com.thlifestore.users.user.util.MediaType;
 import com.thlifestore.users.user.v1.vo.UserVO;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/api/user/v1")
+@Tag(name = "Users")
 public class UserController {
 	
 	@Autowired
@@ -26,28 +34,62 @@ public class UserController {
 	
 	
 	@GetMapping(produces = {MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML,MediaType.APPLICATION_YML})
+	@Operation(summary = "Find all users", description = "Find all users", tags = {"Users"},
+	responses = {@ApiResponse(description = "Sucess", responseCode = "200", content = @Content(
+			mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = UserVO.class)))),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)})
 	public List<UserVO> findAll(){
 		return service.findAll();
 	}
 	
+	
 	@RequestMapping(value = "/{id}",method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML,MediaType.APPLICATION_YML})
+	@Operation(summary = "Find by users", description = "Find by users", tags = {"Users"},
+	responses = {@ApiResponse(description = "Sucess", responseCode = "200", content = @Content(
+			schema = @Schema(implementation = UserVO.class))),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)})
 	public UserVO findById(@PathVariable Long id) {
 		return service.findById(id);
 	}
 	
 	@RequestMapping(method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML,MediaType.APPLICATION_YML},
 			produces = {MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML,MediaType.APPLICATION_YML})
+	@Operation(summary = "creating user using json, xml and yml", description = "creating user using json, xml and yml", tags = {"Users"},
+	responses = {@ApiResponse(description = "Sucess", responseCode = "200", content = @Content(
+			schema = @Schema(implementation = UserVO.class))),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)})
 	public UserVO create(@RequestBody UserVO entity){
 		return service.create(entity);
 	}
 	
 	@PutMapping(consumes = {MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML,MediaType.APPLICATION_YML},
 			produces = {MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML,MediaType.APPLICATION_YML})
+	@Operation(summary = "updating user using json, xml and yml", description = "updating user using json, xml and yml", tags = {"Users"},
+	responses = {@ApiResponse(description = "Updated", responseCode = "200", content = @Content(
+			schema = @Schema(implementation = UserVO.class))),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)})
 	public UserVO update(@RequestBody UserVO entity){
 		return service.update(entity);
 	}
 	
 	@DeleteMapping(value = "/{id}")
+	@Operation(summary = "deleting a user", description = "deleting a user", tags = {"Users"},
+	responses = {@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)})
 	public ResponseEntity<?>delete(@PathVariable Long id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
